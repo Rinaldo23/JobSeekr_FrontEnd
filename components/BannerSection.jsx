fa68d32efd3c82ad3ba315aedc6c80a89f118862
rinaldopankaj <rinaldo.pankaj@example.com>
2023-09-24 17:20:56 +0530

    Code Updated

--- components/BannerSection.jsx
+++ components/BannerSection.jsx
@@ -10,7 +10,7 @@ function BannerSection({ data }) {
 
         <Link href={`/jobs/${data?._id}`} style={{ textDecoration: "none" }}>
             <div className='col-span-2 md:px-6 px-2 md:pt-6 md:h-[600px] pb-4'>
-                <img className='md:h-3/4 w-full object-cover md:rounded-xl rounded-lg' src={data?.image} alt={""} loading='lazy'/>
+                <img className='md:h-3/4 w-full object-cover md:rounded-xl rounded-lg' src={data?.image} alt={""}/>
                 {/* <Image className='md:h-3/4 w-full object-cover md:rounded-xl rounded-lg' width={100} height={100} alt='image' src={data?.image}/> */}
                 <p className='md:mt-4 mt-1 md:text-base text-xs'>{data?.date}</p>
                 <h2 className='md:mt-2 mt-1 md:text-xl text-base'>{data?.title}</h2>
