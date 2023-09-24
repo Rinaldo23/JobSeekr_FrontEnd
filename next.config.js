ae8b1cbf4a551bdd8d783393df9d4b441fa7fec4
rinaldopankaj <rinaldo.pankaj@example.com>
2023-09-24 17:38:41 +0530

    Code Updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  // Set the logo (favicon) for the site
+  head: {
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico", // Path to your favicon
+      },
+    ],
+  },
   images: {
     // domains: ['pexels.com'],
-    unoptimized : true,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>JobSeekr</title>
+        {/* <link rel="icon" type="image/jpg" href="favicon.jpg"></link> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
